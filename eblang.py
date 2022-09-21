b5ba0f4bc843b08ae66ab82962263be8b57a90da
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-09-21 10:33:27 +0400

    Black formatter

--- eblang.py
+++ eblang.py
@@ -3,12 +3,7 @@ import os, sys
 
 
 # Память в режиме интерпритации - словарь из переменных
-memory = {
-    '__name__': {
-        'type': 'const_str',
-        'data': str(__name__)
-    }
-}
+memory = {"__name__": {"type": "const_str", "data": str(__name__)}}
 
 line_ptr = 0
 file_ptr = ""
@@ -31,12 +26,13 @@ def song1(num_bottles=99):
         num_bottles_next = i - 1
         if num_bottles_next == 0:
             num_bottles_next = "no more"
-        print(verse2.format(num_bottles_next,
-                bottle_or_bottles(num_bottles_next)))
+        print(verse2.format(num_bottles_next, bottle_or_bottles(num_bottles_next)))
 
-    print("No more bottles of beer on the wall, no more bottles of beer.\n" \
-           "Go to the store and buy some more, {} bottles of beer on " \
-            "the wall".format(num_bottles))
+    print(
+        "No more bottles of beer on the wall, no more bottles of beer.\n"
+        "Go to the store and buy some more, {} bottles of beer on "
+        "the wall".format(num_bottles)
+    )
 
 
 def _99bottles():
@@ -49,7 +45,7 @@ def suicide():
         # linux
         print("sudo Shutdown PC, please")
         os.system("shutdown now -h")
-        os.system('systemctl poweroff') 
+        os.system("systemctl poweroff")
     elif sys.platform == "darwin":
         # OS X
         print(":/ shutdown your computer manualy, rich bitch!")
@@ -61,7 +57,7 @@ def suicide():
         err(f"Платформа {sys.platform} не поддерживается")
 
 
-# В случае ошибки - аварийный дамп память 
+# В случае ошибки - аварийный дамп память
 def err(message):
     print(f"ERROR:{message}")
     print(f"Memory:{memory}")
@@ -70,85 +66,83 @@ def err(message):
 
 
 # Выполнение команд с аргументами
-def execute(command, param1 = None, param2 = None, param3 = None):
-    if command == 1:    # echo
+def execute(command, param1=None, param2=None, param3=None):
+    if command == 1:  # echo
         if param1 in memory.keys():
-            print(
-                memory.get(param1)['data']
-                )
+            print(memory.get(param1)["data"])
     elif command == 2:  # string
         if param1 in memory.keys():
-            if memory.get(param1)['type'] == "str":
-                print(
-                    param1,     # Имя переменной
-                    param2      # Данные переменной
-                    )
-                memory.get(param1)['data'] = param2
+            if memory.get(param1)["type"] == "str":
+                print(param1, param2)  # Имя переменной  # Данные переменной
+                memory.get(param1)["data"] = param2
             else:
                 err(f"Типы данных не соответствуют: {param2}[{type(param2)}] и string")
         else:
-            memory.update(
-                dict.fromkeys([param1],
-                {
-                    'type': "str", 
-                    'data': param2
-                }
-                    ))
+            memory.update(dict.fromkeys([param1], {"type": "str", "data": param2}))
     elif command == 3:  # int
         if param1 in memory.keys():
-            if memory.get(param1)['type'] == "int":
-                print(
-                    param1,     # Имя переменной
-                    param2      # Данные переменной
-                    )
-                memory.get(param1)['data'] = param2
+            if memory.get(param1)["type"] == "int":
+                print(param1, param2)  # Имя переменной  # Данные переменной
+                memory.get(param1)["data"] = param2
             else:
                 err(f"Типы данных не соответствуют: {param2}[{type(param2)}] и int")
         else:
-            memory.update(
-                dict.fromkeys([param1],
-                {
-                    'type': "int", 
-                    'data': param2
-                }
-                    ))
+            memory.update(dict.fromkeys([param1], {"type": "int", "data": param2}))
     elif command == 4:  # sum
-        if param1 in memory.keys() and param2 in memory.keys() and param3 in memory.keys():
-            if memory.get(param1)['type'] == "int" and \
-              memory.get(param2)['type'] == "int" and \
-              memory.get(param3)['type'] == "int":
+        if (
+            param1 in memory.keys()
+            and param2 in memory.keys()
+            and param3 in memory.keys()
+        ):
+            if (
+                memory.get(param1)["type"] == "int"
+                and memory.get(param2)["type"] == "int"
+                and memory.get(param3)["type"] == "int"
+            ):
                 # param1 = param2 +  param3
-                memory.get(param1)['data'] = int(memory.get(param2)['data']) + \
-                    int(memory.get(param3)['data'])
+                memory.get(param1)["data"] = int(memory.get(param2)["data"]) + int(
+                    memory.get(param3)["data"]
+                )
             else:
-                err(f"Типы данных не соответствуют: {memory.get(param1)['type']}, {memory.get(param2)['type']}, {memory.get(param3)['type']}")
+                err(
+                    f"Типы данных не соответствуют: {memory.get(param1)['type']}, {memory.get(param2)['type']}, {memory.get(param3)['type']}"
+                )
         else:
             err(f"Переменные не найдены: {param1}, {param2}, {param3}")
     elif command == 5:  # sub
-        if param1 in memory.keys() and param2 in memory.keys() and param3 in memory.keys():
-            if memory.get(param1)['type'] == "int" and \
-              memory.get(param2)['type'] == "int" and \
-              memory.get(param3)['type'] == "int":
+        if (
+            param1 in memory.keys()
+            and param2 in memory.keys()
+            and param3 in memory.keys()
+        ):
+            if (
+                memory.get(param1)["type"] == "int"
+                and memory.get(param2)["type"] == "int"
+                and memory.get(param3)["type"] == "int"
+            ):
                 # param1 = param2 - param3
-                memory.get(param1)['data'] = int(memory.get(param2)['data']) - \
-                    int(memory.get(param3)['data'])
+                memory.get(param1)["data"] = int(memory.get(param2)["data"]) - int(
+                    memory.get(param3)["data"]
+                )
             else:
-                err(f"Типы данных не соответствуют: {memory.get(param1)['type']}, {memory.get(param2)['type']}, {memory.get(param3)['type']}")
+                err(
+                    f"Типы данных не соответствуют: {memory.get(param1)['type']}, {memory.get(param2)['type']}, {memory.get(param3)['type']}"
+                )
         else:
             err(f"Переменные не найдены: {param1}, {param2}, {param3}")
     elif command == 10:  # HQ9+
         for i in param1:
-            if i == 'H':
+            if i == "H":
                 print("Hello World!")
-            elif i == 'Q':
+            elif i == "Q":
                 print(param1)
-            elif i == '9':
+            elif i == "9":
                 print(_99bottles())
 
 
 # Парсинг
 def parse(file):
-    f = open(file, 'r', encoding="utf-8")
+    f = open(file, "r", encoding="utf-8")
     programm_raw = []
 
     while True:
@@ -159,15 +153,15 @@ def parse(file):
         if not line:
             f.close
             break
-        
+
         # Если строка не содержит ничего - пропускаем
         if len(line.strip()) < 1:
             continue
 
         # Если строка не содержит комментарий - добавляем её в список
-        if(line.strip()[0] != '#'):
-            programm_raw.append(line.strip().split(':'))
-    
+        if line.strip()[0] != "#":
+            programm_raw.append(line.strip().split(":"))
+
     return programm_raw
 
 
@@ -176,32 +170,32 @@ def analyze(data):
     for i in data:
         command = -1
 
-        if i[0] == 'echo':
+        if i[0] == "echo":
             execute(1, i[1])
-        elif i[0] == 'string':
-            execute(2, i[1].split(',')[0], i[1].split(',')[1])
-        elif i[0] == 'int':
-            execute(3, i[1].split(',')[0], i[1].split(',')[1])
-        elif i[0] == 'sum':
-            execute(4, i[1].split(',')[0], i[1].split(',')[1], i[1].split(',')[2])
-        elif i[0] == 'sub':
-            execute(5, i[1].split(',')[0], i[1].split(',')[1], i[1].split(',')[2])
-        elif i[0] == 'HQ9':
+        elif i[0] == "string":
+            execute(2, i[1].split(",")[0], i[1].split(",")[1])
+        elif i[0] == "int":
+            execute(3, i[1].split(",")[0], i[1].split(",")[1])
+        elif i[0] == "sum":
+            execute(4, i[1].split(",")[0], i[1].split(",")[1], i[1].split(",")[2])
+        elif i[0] == "sub":
+            execute(5, i[1].split(",")[0], i[1].split(",")[1], i[1].split(",")[2])
+        elif i[0] == "HQ9":
             execute(10, i[1])
-        elif i[0] == 'meminfo':
+        elif i[0] == "meminfo":
             print(memory)
-        elif i[0] == 'quine':
+        elif i[0] == "quine":
             f = open(file_ptr, "r", encoding="utf-8")
             print(f.read())
             f.close
-        elif i[0] == 'suicide':
+        elif i[0] == "suicide":
             suicide()
         else:
             err(f"Инструкция: {i[0]}, не распознанна")
 
 
 # Обработка аргументов
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         for i in range(1, len(sys.argv)):
             file_ptr = sys.argv[i]
